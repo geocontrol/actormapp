31d20731e11da37211b3468b47fa7e9308ef931f
Mark Simpkins <mark@geekyoto.com>
2016-05-06 21:16:09 +0100

    just catching stuff up

--- Support/deploy.bat
+++ Support/deploy.bat
@@ -1,12 +1,12 @@
 SRC_ROOT=~
-SRC_DIR=$SRC_ROOT/aladmin
+SRC_DIR=$SRC_ROOT/actormapp
 if [ -d "$SRC_DIR" ]; then
   cd $SRC_DIR
   git pull origin master
   npm update
 else
   mkdir -p $SRC_ROOT
-  git clone https://github.com/geekyoto/AlwaysListeningAdmin.git $SRC_DIR
+  git clone https://github.com/geocontrol/actormapp.git $SRC_DIR
   cd $SRC_DIR
   npm install
 fi
